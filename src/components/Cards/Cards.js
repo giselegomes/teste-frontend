bc68e4dc6e80839c86f515ae09e2672b397d9f17
Gisele Gomes <gselegomes@gmail.com>
2023-03-07 21:58:03 -0300

    feat: adds pagination

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,122 +1,51 @@
 import React from "react";
-import "./Cards.css"
+import Pagination from "../Pagination/Pagination";
+import "./Cards.css"; 
+import { useState, useEffect } from "react";
 
 const Cards = () => {
 
+    const [cards, setCards] = useState([])
+    const [cardsPerPage, setItemsPerPage] = useState(25)
+    const [currentPage, setCurrentPage] = useState(0)
+    
+
+    const pages = Math.ceil(cards.length / cardsPerPage)
+    const startIndex = currentPage + cardsPerPage;
+    const endIndex = startIndex + cardsPerPage;
+    const currentCards = cards.slice(startIndex, endIndex)
+
+    useEffect(() => {
+        //consumir a api ...
+        
+        fetch('https://compras.dados.gov.br/contratos/v1/contratos.json?uasg=153229')
+        .then(response => response.json())
+        .then(data => setCards(data._embedded.contratos))
+    }, []
+    )
+
     return (
         <>
-            <section className="cards">
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
-                    <div className="cards__item">
-                        <p>uasg: </p>
-                        <p>modalidade: </p>
-                        <p>número do aviso da licitação: </p>
-                        <a href="google.com">
-                            visualizar
-                            <span class="material-symbols-outlined">
-                                open_in_new
-                            </span>
-                        </a>
-                    </div>
+            <section className="content_cards">
+                <section className="cards">
+                    {currentCards.map(card => {
+                        return (
+                            <div className="cards__item">
+                            <p>uasg: { card.uasg }</p>
+                            <p>modalidade: { card.modalidade_licitacao }</p>
+                            <p>número do aviso da licitação: { card.numero_aviso_licitacao }</p>
+                            <a href={`https://compras.dados.gov.br${card._links.self.href}`} target="_blank">
+                                visualizar
+                                <span class="material-symbols-outlined">
+                                    open_in_new
+                                </span>
+                            </a>
+                        </div> 
+                        )
+                    })}
                 </section>
+            <Pagination pages={pages} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+            </section>
         </>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import Cards from "../../components/Cards/Cards";
 import "./Home.css"
 
 function Home() {
+
     return (
         <>
             <Navbar />
